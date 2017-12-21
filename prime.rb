a1cc6db32b8ba476f3826f6d4d1d5700e4d446dd
Irene Meisel <irene@irenemeisel.net>
2017-12-21 07:29:37 +0000

    message herelearn open prime-ruby-web-010818
     Please enter the commit message for your changes. Lines starting

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n) # number evenly divisible by itself and 1
+  return FALSE if n<=1
+
+     for i in 2..n-1
+      if n % i == 0  #if remainder of number divided by any number leading up to it is 0 then it's evenly divided by that number so NOT prime
+    return FALSE
+  end
+ end
+
+ TRUE
+
+end
